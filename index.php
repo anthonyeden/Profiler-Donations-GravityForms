93dac8046f3497a864a99545902faf74eb9faa92
Anthony Eden <anthony@mediarealm.com.au>
2018-05-19 22:25:01 +1000

    Bump version 1.3.0

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Profiler Donations - Gravity Forms Add-On
 Plugin URI: http://mediarealm.com.au/
 Description: Integrates Gravity Forms with Profiler, enabling donation data to be sent directly to Profiler.
-Version: 1.2.4
+Version: 1.3.0
 
 Author: Media Realm
 Author URI: http://www.mediarealm.com.au/
