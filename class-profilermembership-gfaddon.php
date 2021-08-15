b40f73ff0b919fab813a9538fcfb99c81d86d730
Anthony Eden <anthony@mediarealm.com.au>
2021-08-15 20:25:19 +1000

    Membership: Allow the payment to be processed as a pledge, instead of an instant payment

--- class-profilermembership-gfaddon.php
+++ class-profilermembership-gfaddon.php
@@ -29,6 +29,64 @@ class GFProfilerMembership extends GFProfilerDonate {
 
     public function init() {
         parent::init();
+
+        add_filter("gform_validation", array($this, "remove_payment_vaidators"), 1);
+    }
+
+    public function feed_settings_fields_custom() {
+        // Add extra fields for processing memberships later
+
+        $userdefinedfields = self::$_instance->userDefinedFields();
+
+        $fields = parent::feed_settings_fields_custom();
+
+        foreach($fields as $fieldKey => $fieldVal) {
+            if($fieldVal['label'] == "Use Profiler As A Gateway?") {
+                $fields[$fieldKey]['choices'][] = array(
+                    'label'         => 'Manual - Profiler will Process Payments, manually later on via a Pledge',
+                    'value'         => 'manual',
+                );
+            }
+        }
+
+        $fields[] = array(
+            "label" => 'UDF: Stored Card Number',
+            "type" => "select",
+            "name" => "profilerdonation_userdefined_storedcard_number",
+            "required" => false,
+            "tooltip" => "Pick the Profiler User Defined Field you wish to store the card number in",
+            "choices" => $userdefinedfields,
+        );
+
+        $fields[] = array(
+            "label" => 'UDF: Stored Card Expiry',
+            "type" => "select",
+            "name" => "profilerdonation_userdefined_storedcard_expiry",
+            "required" => false,
+            "tooltip" => "Pick the Profiler User Defined Field you wish to store the card expiry in",
+            "choices" => $userdefinedfields,
+        );
+
+        $fields[] = array(
+            "label" => 'UDF: Stored Card CCV',
+            "type" => "select",
+            "name" => "profilerdonation_userdefined_storedcard_ccv",
+            "required" => false,
+            "tooltip" => "Pick the Profiler User Defined Field you wish to store the card CCV in",
+            "choices" => $userdefinedfields,
+        );
+
+        $fields[] = array(
+            "label" => 'UDF: Stored Card Name',
+            "type" => "select",
+            "name" => "profilerdonation_userdefined_storedcard_name",
+            "required" => false,
+            "tooltip" => "Pick the Profiler User Defined Field you wish to store the card name in",
+            "choices" => $userdefinedfields,
+        );
+
+        return $fields;
+
     }
 
     public function process_feed_custom($feed, $entry, $form, $postData, $fromValidatorProcessPFGateway = false, $forceSendCard = false) {
@@ -37,11 +95,21 @@ class GFProfilerMembership extends GFProfilerDonate {
 
         $postData = parent::process_feed_custom($feed, $entry, $form, $postData, $fromValidatorProcessPFGateway, true);
 
+        $payLater = false;
+
         if($feed['meta']['profilerdonation_useasgateway'] == "true" && $fromValidatorProcessPFGateway == true) {
             $useAsGateway = true;
 
         } elseif($feed['meta']['profilerdonation_useasgateway'] !== "true" && $fromValidatorProcessPFGateway == true) {
             // This shouldn't happen. Let's catch it just in case.
+            return false;
+
+        } elseif($feed['meta']['profilerdonation_useasgateway'] == "manual" && $fromValidatorProcessPFGateway == false) {
+            $useAsGateway = false;
+            $payLater = true;
+
+        } elseif($feed['meta']['profilerdonation_useasgateway'] == "manual" && $fromValidatorProcessPFGateway == true) {
+            // Shouldn't happen
             return false;
 
         } else {
@@ -56,8 +124,67 @@ class GFProfilerMembership extends GFProfilerDonate {
 
         $postData['pledgeamount'] = $postData['amount'];
 
+        if($payLater === true) {
+            $cardDetails = $this->getCardDetails($form);
+
+            if($feed['meta']['profilerdonation_userdefined_storedcard_number'] !== "") {
+                $postData['userdefined' . $feed['meta']['profilerdonation_userdefined_storedcard_number']] = $cardDetails['number'];
+            }
+
+            if($feed['meta']['profilerdonation_userdefined_storedcard_expiry'] !== "") {
+                $postData['userdefined' . $feed['meta']['profilerdonation_userdefined_storedcard_expiry']] = $cardDetails['expiry_month'] . " " . $cardDetails['expiry_year'];
+            }
+
+            if($feed['meta']['profilerdonation_userdefined_storedcard_ccv'] !== "") {
+                $postData['userdefined' . $feed['meta']['profilerdonation_userdefined_storedcard_ccv']] = $cardDetails['ccv'];
+            }
+
+            if($feed['meta']['profilerdonation_userdefined_storedcard_name'] !== "") {
+                $postData['userdefined' . $feed['meta']['profilerdonation_userdefined_storedcard_name']] = $cardDetails['name'];
+            }
+
+            unset($postData['cardtype']);
+            unset($postData['cardnumber']);
+            unset($postData['ccv']);
+            unset($postData['cardexpiry']);
+        }
+
         return $postData;
 
+    }
+
+    public function remove_payment_vaidators($gform_validation_result) {
+        // Attempts to remove other payment gateways. This is a bit gateway-specific, and will not work universally
+
+        if(!$gform_validation_result['is_valid']) {
+            // If it's already failed validation...
+            return $gform_validation_result;
+        }
+
+        $form = $gform_validation_result['form'];
+        $entry = GFFormsModel::create_lead($form);
+        $feed = $this->get_feed_instance($form, $entry);
+
+        if(!$feed) {
+            return $gform_validation_result;
+        }
+
+        if($feed['meta']['profilerdonation_useasgateway'] !== "manual") {
+            // If we're not in manual mode, we don't need to continue here
+            return $gform_validation_result;
+        }
+
+        // Remove SecurePay payment filter
+        if(class_exists('GFSPPlugin')) {
+            remove_filter('gform_validation', array(GFSPPlugin::getInstance(), 'gformValidation'), 100);
+        }
+
+        // Remove EWay payment filter
+        if(class_exists('GFEwayPlugin')) {
+            remove_filter('gform_validation', array(GFEwayPlugin::getInstance(), 'gformValidation'), 100);
+        }
+
+        return $gform_validation_result;
     }
 
 }
